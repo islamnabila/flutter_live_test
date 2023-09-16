6116c92316fd5f75f30dee8e3698e7b5115c9803
islamnabila <81310064+islamnabila@users.noreply.github.com>
2023-09-16 23:16:20 +0600

    live test 6 added

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,40 @@ class MyHomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("My Shopping List", style: TextStyle(fontSize: 22),),
+        centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: Icon(Icons.shopping_cart),
+          )
+        ],
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: Icon(Icons.shopping_basket),
+            title: Text("Apples", style: TextStyle(fontSize: 18),),
+          ),
+          ListTile(
+            leading: Icon(Icons.shopping_basket),
+            title: Text("Bananas",style: TextStyle(fontSize: 18),),
+          ),
+          ListTile(
+            leading: Icon(Icons.shopping_basket),
+            title: Text("Bread",style: TextStyle(fontSize: 18),),
+          ),
+          ListTile(
+            leading: Icon(Icons.shopping_basket),
+            title: Text("Milk",style: TextStyle(fontSize: 18),),
+          ),
+          ListTile(
+            leading: Icon(Icons.shopping_basket),
+            title: Text("Eggs",style: TextStyle(fontSize: 18),),
+          )
+        ],
+      ),
 
     );
   }
