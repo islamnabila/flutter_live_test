9bcd0a26bad94a39f7456e2823e675f634af418d
islamnabila <81310064+islamnabila@users.noreply.github.com>
2023-09-29 23:22:28 +0600

    live test 8 added

---
+++ lib/homepage.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyHome extends StatefulWidget {
+  const MyHome({super.key});
+
+  @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  @override
+  Widget build(BuildContext context) {
+    if (MediaQuery.of(context).orientation == Orientation.portrait) {
+      return ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Card(
+            child: Container(
+              width: 150,
+              height: 150,
+              child: Image.network(
+                'https://via.placeholder.com/150',
+              ),
+            ),
+          );
+        },
+      );
+    } else {
+      return GridView.builder(
+        itemCount: 10,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+        ),
+        itemBuilder: (context, index) {
+          return Card(
+            child: Container(
+              margin: EdgeInsets.only(top: 10),
+              height: 150,
+              width: 150,
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Image.network(
+                  'https://via.placeholder.com/150',
+                ),
+              ),
+            ),
+          );
+        },
+      );
+    }
+  }
+}
